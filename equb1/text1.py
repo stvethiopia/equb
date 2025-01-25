e398b2d486452a034a781f9ea392e20098991115
Sam Rega <stvethiopia@gmail.com>
2025-01-24 21:41:45 -0500

    v1.1

---
+++ equb1/text1.py
@@ -0,0 +1,7 @@
+import random
+
+# Generate a random integer between a specified range
+random_number = random.randint(1, 30)  # Change the range as needed
+
+# Print the random number
+print("Random Number:", random_number)
