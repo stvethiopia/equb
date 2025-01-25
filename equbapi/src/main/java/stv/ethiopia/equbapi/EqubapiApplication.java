21928af6465992b2b2078df29fdafde849f17792
Sam Rega <stvethiopia@gmail.com>
2025-01-24 22:14:58 -0500

    api added

---
+++ equbapi/src/main/java/stv/ethiopia/equbapi/EqubapiApplication.java
@@ -0,0 +1,13 @@
+package stv.ethiopia.equbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqubapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqubapiApplication.class, args);
+	}
+
+}
---
+++ equbapi/src/test/java/stv/ethiopia/equbapi/EqubapiApplicationTests.java
@@ -0,0 +1,13 @@
+package stv.ethiopia.equbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqubapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
